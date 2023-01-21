4ed5ffdc52d19960b9102e87a6c9943f5d03aa47
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2023-01-20 21:05:48 -0500

    wording change

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,7 +7,7 @@ export const AboutPage = () => {
         <p>I'm a software engineer and avid reader</p>
         <p>
             I love learning new programming languages and am currently interested in mobile development.
-            I enjoy outdoor activities, reading about personal development and finance, and do cross-fit.
+            I enjoy outdoor activities, reading about personal development and finance, and doing cross-fit.
         </p>
         </>
     )
